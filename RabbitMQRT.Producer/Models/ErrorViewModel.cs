3818d9a101002f45b72a5d305d0dd0987187f2f5
cihan Çakır <cihancakir@trakya.edu.tr>
2020-01-03 17:12:26 +0300

    Bismillah

---
+++ RabbitMQRT.Producer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RabbitMQRT.Producer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
